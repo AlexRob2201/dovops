e9677f72bfb91f0306dd0c5c19c8ec7378d72878
Alex <buxenko2201@gmail.com>
2023-11-20 19:05:41 +0200

    add picture for HT

--- HT/HT-17/main.py
+++ HT/HT-17/main.py
@@ -6,7 +6,7 @@ import logging
 class LinkParser():
     def __init__(self, verbose=False):
         self.content = None
-        self.data = {}
+        self.data = []
         self.verbose = verbose
 
         logging.basicConfig(level=logging.INFO)
@@ -16,7 +16,7 @@ class LinkParser():
     def get_content(self, link):
         self.content = requests.get(link)
         if self.verbose:
-            self.logger.debug(f"Response from {link}: {self.content.content}")
+            self.logger.info(f"Response from {link}")
 
     def get_json(self):
         todos = json.loads(self.content.text)
@@ -27,21 +27,24 @@ class LinkParser():
             json_data = self.get_data_from_json()
             with open(destination, 'w') as destination_file:
                 json.dump(json_data, destination_file, indent=2)
-            self.logger.info(f"Data successfully saved to {destination}")
+            if self.verbose:    
+                self.logger.info(f"Data successfully saved to {destination}")
         except Exception as e:
             self.logger.error(f"Error: {e}")
 
     def get_data_from_json(self):
         json_data = self.get_json()
+        i = 1
         for data in json_data:
             user_id = data.get('userId')
             if user_id is not None:
-                self.data[f"User : {user_id}"] = str(user_id)
+                self.data.append(f"User {i}: {user_id}")
+                i+=1
                 if self.verbose:
-                    self.logger.debug(f"Found userId: {user_id}")
+                    self.logger.info(f"Found userId: {user_id}")
         return self.data
 
 if __name__ == "__main__":
-    parser = LinkParser(verbose=False)  # Передаємо verbose=True, щоб увімкнути режим логування
+    parser = LinkParser(verbose=False)  
     parser.get_content('https://jsonplaceholder.typicode.com/posts')
     parser.save_to_my_destination('destination.json')
