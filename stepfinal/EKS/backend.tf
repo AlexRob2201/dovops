cab0f16ef724e98582499d1c75e5504ec0c6dcff
vagrant <vagrant@Devops>
2024-05-07 09:10:40 +0000

    Step final, stage 1 GitHub action and workflow

---
+++ stepfinal/EKS/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    AWS_REGION = var.region
+    bucket         = "danit-devops-tf-state"
+    # Example
+    key            = "eks/terraform.tfstate"
+    encrypt        = true
+    # Example
+    #dynamodb_table = "lock-tf-eks"
+    dynamodb_table = "bukhenko-step" 
+    # dynamo key LockID
+    # Params tekan from -backend-config when terraform init
+    #region = 
+    #profile = 
+  }
+}
+
